f1e23a32d0e1e83d5fa0620997065ca85127a3d1
marvinobig <88102362+marvinobig@users.noreply.github.com>
2023-10-05 16:51:31 +0100

    remove unnecessary require statement

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,7 +2,6 @@
 $root = $_SERVER['DOCUMENT_ROOT'];
 require_once "$root/classes/SQL.php";
 require_once "$root/classes/Utilities.php";
-require_once "$root/classes/CustomerDTO.php";
 
 $db = new SQL('ExDatabase10', 'Learner10', 'BalticSQL1');
 $balticDb = $db->dbConnect();
