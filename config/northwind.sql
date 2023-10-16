5e2f5cd106be7ce2e07efb7d2fe7a3cf23014adf
marvinobig <88102362+marvinobig@users.noreply.github.com>
2023-10-16 08:08:15 +0100

    create sql script file

---
+++ config/northwind.sql
@@ -0,0 +1,13 @@
+CREATE TABLE 'Customers' (
+    CustomerID PRIMARY KEY AUTO
+    CompanyName
+    ContactName
+    ContactTitle
+    Address
+    City
+    PostalCode
+    Country
+    Phone
+    Fax
+    Action
+)
