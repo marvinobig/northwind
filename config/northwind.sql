0dbeeb84b4ce00f282d7a771f0adb7027ded72d3
marvinobig <88102362+marvinobig@users.noreply.github.com>
2023-10-16 08:16:32 +0100

    add create table statement for website db

--- config/northwind.sql
+++ config/northwind.sql
@@ -1,13 +1,12 @@
-CREATE TABLE 'Customers' (
-    CustomerID PRIMARY KEY AUTO
-    CompanyName
-    ContactName
-    ContactTitle
-    Address
-    City
-    PostalCode
-    Country
-    Phone
-    Fax
-    Action
+CREATE TABLE IF NOT EXISTS 'Customers' (
+    CustomerID INT PRIMARY KEY,
+    CompanyName VARCHAR(50),
+    ContactName VARCHAR(50),
+    ContactTitle VARCHAR(50),
+    Address VARCHAR,
+    City VARCHAR(50),
+    PostalCode VARCHAR(50),
+    Country VARCHAR(50),
+    Phone VARCHAR(50),
+    Fax VARCHAR(50)
 )
